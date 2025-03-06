4a5c29344c54231b8e5c69457b36d0ced8822c27
Denickrono <123111542+Denickrono@users.noreply.github.com>
2025-03-06 12:55:59 +0300

    Add files via upload

---
+++ sketch_jan8g.ino
@@ -0,0 +1,31 @@
+#include <LiquidCrystal.h>
+#include "DHT.h" // Call the DHT library
+#define DHTPIN 8 // Pin connected to DHT
+
+#define DHTTYPE DHT11 // Define the type of DHT module
+DHT dht(DHTPIN, DHTTYPE); // Command to the DHT.h library
+void setup() {
+ dht.begin(); // Start the sensor
+ 
+ Serial.begin(9600);
+}
+void loop() {
+ float h = dht.readHumidity(); // Value for humidity
+ float t = dht.readTemperature(); // Value for temperature
+ //t = t * 9 / 5 + 32; // Change reading from Celsius to Fahrenheit
+ if (isnan(t) || isnan(h)) { // Check that DHT sensor is working
+ 
+ Serial.print("Failed to read from DHT"); // If DHT is not working, 
+ // display this
+ } else { // Otherwise show the readings on the screen
+ 
+ Serial.print("humidity is ");
+ Serial.print(h);
+ Serial.println("%");
+ 
+ Serial.print("Temperature is");
+ Serial.print(t);
+ 
+ //huiujn,mlcd.print("f");
+ }
+}
